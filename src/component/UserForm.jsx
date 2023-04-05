e7a20ebf1cd7389f1276bb4edf0a84db732caab3
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-04-05 20:01:52 +0700

    create add user data and data list form components

---
+++ src/component/UserForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const UserForm = ({ onUserAdd }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onUserAdd({ name, email })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+      </div>
+      <button>Add User</button>
+    </form>
+  )
+}
